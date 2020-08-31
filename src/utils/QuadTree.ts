7b97634a108a75c393275fff6cc3730d54756dac
will <will@rushdigital.co.nz>
2020-09-01 00:26:35 +1200

    Add quad tree search

--- src/utils/QuadTree.ts
+++ src/utils/QuadTree.ts
@@ -1,5 +1,11 @@
 import { Vector2, Vector3 } from "three";
 
+const intersectRectangle = (point: Vector2, min: Vector2, max: Vector2) => {
+  return (
+    point.x >= min.x && point.x <= max.x && point.y >= min.y && point.y <= max.y
+  );
+};
+
 export default class QuadTree<T> {
   map: { [id: string]: QuadChunk<T> } = {};
   size = 32;
@@ -14,6 +20,63 @@ export default class QuadTree<T> {
     const origin = this.getOrigin(position);
     const chunk = this.getChunk(origin);
     return chunk.get(position);
+  }
+
+  find(position: Vector3, distance: number): T[] {
+    const origins = this.getOriginsToSearch(position, distance);
+    const results: T[] = [];
+
+    for (const origin of origins) {
+      const chunk = this.getChunk(origin);
+      if (chunk == null) {
+        continue;
+      }
+      for (const id in chunk.map) {
+        const entry = chunk.map[id];
+        const dis = entry.position.distanceTo(position);
+        if (dis < distance) {
+          results.push(entry.value);
+        }
+      }
+    }
+
+    return results;
+  }
+
+  getOriginsToSearch(position: Vector3, distance: number) {
+    const origin = this.getOrigin(position);
+
+    const origins = [];
+    for (var i = -1; i <= 1; i++) {
+      for (var j = -1; j <= 1; j++) {
+        const chunk = origin
+          .clone()
+          .add(new Vector2(i, j).multiplyScalar(this.size));
+
+        if (i === 0 && j === 0) {
+          origins.push(chunk);
+        } else {
+          const min = chunk.clone().add(new Vector2(-distance, -distance));
+          const rect = {
+            min,
+            max: min
+              .clone()
+              .add(new Vector2(this.size, this.size))
+              .add(new Vector2(distance, distance)),
+          };
+          const result = intersectRectangle(
+            new Vector2(position.x, position.z),
+            rect.min,
+            rect.max
+          );
+          if (result) {
+            origins.push(chunk);
+          }
+        }
+      }
+    }
+
+    return origins;
   }
 
   private getOrigin(position: Vector3) {
@@ -43,16 +106,24 @@ export default class QuadTree<T> {
 }
 
 class QuadChunk<T> {
-  map: { [key: string]: T } = {};
+  map: { [key: string]: Entry<T> } = {};
   set(position: Vector3, value: T) {
     const key = this.getKey(position);
-    this.map[key] = value;
+    this.map[key] = {
+      position,
+      value,
+    };
   }
   get(position: Vector3): T | undefined {
     const key = this.getKey(position);
-    return this.map[key];
+    return this.map[key].value;
   }
   private getKey(position: Vector3) {
     return `${position.x},${position.y},${position.z}`;
   }
+}
+
+interface Entry<T> {
+  position: Vector3;
+  value: T;
 }
--- src/utils/QuatTree.spec.ts
+++ src/utils/QuatTree.spec.ts
@@ -1,9 +1,37 @@
 import QuadTree from "./QuadTree";
-import { Vector3 } from "three";
+import { Vector3, Vector2 } from "three";
 
 test("get", () => {
   const quadTree = new QuadTree();
   quadTree.set(new Vector3(33, 34, 35), 123);
   const result = quadTree.get(new Vector3(33, 34, 35));
   expect(result).toEqual(123);
+});
+
+test("getOriginsToSearch", () => {
+  const quadTree = new QuadTree();
+  const origins = quadTree
+    .getOriginsToSearch(new Vector3(32, 0, 32), 1)
+    .map((x) => `${x.x},${x.y}`);
+
+  expect(origins).toContain("0,0");
+  expect(origins).toContain("0,32");
+  expect(origins).toContain("32,0");
+  expect(origins).toContain("32,32");
+});
+
+test("search within range", () => {
+  const quadTree = new QuadTree();
+  quadTree.set(new Vector3(33, 33, 33), 1);
+  const results = quadTree.find(new Vector3(32, 32, 32), 2);
+  expect(results).toHaveLength(1);
+  const result = results[0];
+  expect(result).toEqual(1);
+});
+
+test("search outside range", () => {
+  const quadTree = new QuadTree();
+  quadTree.set(new Vector3(33, 33, 33), 1);
+  const results = quadTree.find(new Vector3(32, 32, 32), 1);
+  expect(results).toHaveLength(0);
 });
