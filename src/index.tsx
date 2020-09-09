3ab82dae9965eaf4e8c6f6d18f7574957aa98192
will <will@rushdigital.co.nz>
2020-09-09 21:50:01 +1200

    Build production with parcel 2

--- src/index.tsx
+++ src/index.tsx
@@ -3,3 +3,7 @@ import React from "react";
 import App from "./App";
 
 ReactDom.render(<App />, document.getElementById("root"));
+
+if (process.env.NODE_ENV === 'development') {
+    console.log("It's a development!");
+}
